ff4a8773b37eb3c7f52c638076ef85b15f07872b
sukhmani <sukhmani@bellevuecollege.edu>
2021-08-21 20:33:34 -0700

    a

--- day11/reactiveForm/src/app/app.module.ts
+++ day11/reactiveForm/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+ 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home.component';
+import { BatmanComponent } from './batman.component';
+import { IronmanComponent } from './ironman.component';
+import { SupermanComponent } from './superman.component';
+import { SpidermanComponent } from './spiderman.component';
+import { NotfoundComponent } from './notfound.component';
+import { RouterModule, Routes } from '@angular/router';
+ 
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BatmanComponent,
+    IronmanComponent,
+    SupermanComponent,
+    SpidermanComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, RouterModule.forRoot([
+      { path : '', component : HomeComponent },
+      { path : 'batman', component : BatmanComponent },
+      { path : 'superman', component : SupermanComponent },
+      { path : 'ironman', component : IronmanComponent },
+      { path : 'spiderman', component : SpidermanComponent },
+      { path : 'hulk', component : NotfoundComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
