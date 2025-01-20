2af11b584f3ed3bd82d2108d1ad7a952c66f5ff5
sukhmani <sukhmani@bellevuecollege.edu>
2025-01-20 00:40:40 +0000

    resume

---
+++ hello/src/App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import Resume from './Resume';
+
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
---
+++ hello/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React  from "react";
+import App from "../App";
+import Greet from './components/Greet'
+
+function Greet(){
+    return <h1>Hello Vishwas</h1>
+}
+
+export default App;
