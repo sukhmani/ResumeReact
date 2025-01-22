661a96fb88a3ceac97d2d9fac868daa5e5707a9b
sukhmani <sukhmani@bellevuecollege.edu>
2025-01-22 04:40:05 +0000

    img

--- src/Profile.js
+++ src/Profile.js
@@ -7,11 +7,13 @@ function Profile() {
     bio: 'Software Developer from Seattle, WA.',
     linkedin: 'https://www.linkedin.com/in/sukhmani-thukral/',
     contact: 'thukralsukhmani@cityuniversity.edu',
-    phone: '(206) 225-8829'
+    phone: '(206) 225-8829',
+    avatar: 'https://github.com/sukhmani/ResumeReact/raw/main/Screenshot%202025-01-14%20114954.png' // Use the raw URL of the image
   };
 
   return (
     <div className="profile">
+      <img src={user.avatar} alt="Avatar" />
       <h2>{user.name}</h2>
       <p>{user.bio}</p>
       <p>Email: 
